ac48ec274097488593c83322ecbbf1a2abd051e3
Henrique Silva <hensilva@users.noreply.github.com>
2020-09-30 14:51:57 -0300

    Add files via upload

---
+++ cdap-distributions/src/dataproc/install.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+CDAP_BRANCH=release/6.2
+CDAP_VERSION=6.2.0-1
+CHEF_VERSION=14.15.6 # 14.15.6 e 13.12.14 ok. Testar 16.5.77 ou 15.14.0 (a partir de 16 requer supermerket)
+CHEF_COOKBOOK=3.3.3
+COOKBOOK_DOWNLOAD=v1 # v1 ok. Testar v2
+EXPLORE_ENABLED='false'
+SERVICE_DELAY=240
+DASHBOARD_PORT=8000
+CDAP_APT_REPO_URL=https://repository.cdap.io/ubuntu/precise/amd64/cdap/6.2
+GIT_REPO=https://github.com/hensilva/cdap.git # original https://github.com/cdapio/cdap.git
+SPARK_COMPAT=spark2_2.11
+
+__tmpdir="/tmp/cdap_install_temp"
+__ipaddr=$(hostname -i)
+__gitdir="${__tmpdir}/cdap"
+__packerdir="${__gitdir}/cdap-distributions/src/packer/scripts"
+__cdap_site_dir="${__gitdir}/cdap-distributions/src/dataproc"
+
+test -d ${__tmpdir} && sudo rm -rf ${__tmpdir}
+
+# Install git
+sudo apt-get install -y git
+
+# Install chef
+mkdir -p ${__tmpdir}
+curl -L -o ${__tmpdir}/install.sh https://www.chef.io/chef/install.sh && sudo bash ${__tmpdir}/install.sh -v ${CHEF_VERSION}
+
+# Clone CDAP repo
+git clone --depth 1 --branch ${CDAP_BRANCH} ${GIT_REPO} ${__gitdir}
+
+# Setup cookbook repo
+test -d /var/chef/cookbooks && sudo rm -rf /var/chef/cookbooks
+sudo ${__packerdir}/cookbook-dir.sh
+
+# Install cookbooks via knife
+mkdir -p ${__tmpdir}/cookbook-download
+cd ${__tmpdir}/cookbook-download
+sudo chmod +x ${__packerdir}/cookbook-setup-${COOKBOOK_DOWNLOAD}.sh
+sudo ${__packerdir}/cookbook-setup-${COOKBOOK_DOWNLOAD}.sh
+
+# Copy recipe workarounds
+sudo cp -p ${__cdap_site_dir}/cdap${CHEF_COOKBOOK}/recipes/*.rb /var/chef/cookbooks/cdap/recipes
+
+# Create chef json configuration
+sed \
+  -e "s#{{DASHBOARD_PORT}}#${DASHBOARD_PORT}#" \
+  -e "s#{{CDAP_VERSION}}#${CDAP_VERSION}#" \
+  -e "s#{{CDAP_APT_REPO_URL}}#${CDAP_APT_REPO_URL}#" \
+  -e "s#{{EXPLORE_ENABLED}}#${EXPLORE_ENABLED}#" \
+  -e "s#{{ROUTER_IP_ADDRESS}}#${__ipaddr}#" \
+  -e "s#{{SPARK_COMPAT}}#${SPARK_COMPAT}#" \
+  ${__cdap_site_dir}/cdap-conf.json > ${__tmpdir}/generated-conf.json
+
+# Install/Configure CDAP
+sudo chef-solo -o 'recipe[cdap::fullstack]' -j ${__tmpdir}/generated-conf.json
+
+### TODO: Temporary Hack to workaround CDAP-4089
+sudo rm -f /opt/cdap/kafka/lib/log4j.log4j-1.2.14.jar
+
+### TODO: Ensure Kafka directory is available until caskdata/cdap_cookbook#187 is merged and released
+sudo su - -c "mkdir -p /mnt/cdap/kafka-logs && chown -R cdap /mnt/cdap"
+
+# Start CDAP Services
+for i in /etc/init.d/cdap-*; do
+  __svc=$(basename ${i})
+  echo ${__svc}
+  sudo service ${__svc} start
+done
